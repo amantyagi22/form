11e2e1f8e1b8c9a89156411fe35c1fef3dbf3ebf
vratant11 <vratant2012061@gmail.com>
2022-04-03 17:54:21 +0530

    bootstrap initiated

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
-
+import React from "react";
 
 function Reg() {
   return (
     <>
-    <div className='container'>
-      <div className='image1'>
+      <div className="cont">
+        <div className="image1"></div>
+        <div className="image2">
+          <h1 className="text">Hey! Get Yourself Registered</h1>
+          <div>
+            <form className="container">
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+              </div>
+
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
-      <div className='image2'>
-      </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Reg
-
+export default Reg;
