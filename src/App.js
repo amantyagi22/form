6c3b63073cd020828a39d1a0c8d89e2baebff7f3
vratant11 <vratant2012061@gmail.com>
2022-04-03 16:59:47 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Reg from './components/Reg.jsx';
+
+function App() {
+  return (
+    <>
+    <Reg/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Reg.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function Reg() {
+  return (
+    <>
+    <div className='container'>
+      <div className='image1'>
+      </div>
+      <div className='image2'>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Reg
+
