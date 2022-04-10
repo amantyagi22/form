ee6fdf92bd6e11585373be354ebbe93816e6f61b
Prakhar2010030 <prakhar2010030kumar@gmail.com>
2022-04-10 17:02:15 +0530

    new branch

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,66 +1,75 @@
 import React, {useEffect, useState } from "react";
-// import {useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import validation from './validation';
-import Axios from "axios";
+import axios from "axios";
 function Reg() {
-    // const navigate = useNavigate();
-  const[submitform,setSubmitform] = useState(false);
-  const [values, setValues] = useState({
-    Name: "",
-    Rollno: "",
-    Contactno: "",
-    Email: "",
-    Gender: "",
-    Year: "",
-    Branch: "",
-  });
-  const [errors,setErrors]=useState({});
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-    
-  };
-  useEffect(()=>{
-    if(Object.keys(errors).length === 0){
-        setSubmitform(true);
-    }
-},[errors]);
-  const url = "https://workshopregistration.herokuapp.com/api/users/register";
-  const handleFormSubmit = (event) => {
-    console.log(values);
-    event.preventDefault();
-    setErrors(validation(values));
-    Axios.post(url, {
-      Name: values.Name,
-      Rollno: values.Rollno,
-      Email: values.Email,
-      Contactno: values.Contactno,
-      Gender: values.Gender,
-      Year: values.Year,
-      Branch: values.Branch,
-    }).then((res) => {
-      console.log(res.data);
-    });
-    Axios.post(
-      "https://workshopregistration.herokuapp.com/api/users/confirmemail",
-      {
-        Email: values.Email,
-      }
-    ).then((res) => {
-      console.log(res.data);
-    });
-  };
   return (
     <>
-      <div className="cont">
-        <div className="image1"></div>
-        <div className="image2">
-          <h2 className="hed my-4">Hey! Get Yourself Registered</h2>
+      <div className="form_container">
+        <div className="image_left"></div>
+        <div className="image_right">
+          <h2 className="left_heading my-4">Hey! Get Yourself Registered</h2>
+          <form className="input">
+        <div className="input_container">
+          <input type="text"  className="input_field" placeholder="Name"/>
+        </div>
+        <div className="input_container">
+          <input type="text"  className="input_field" placeholder="University Roll No."/>
+        </div>
+        <div className="input_container">
+          <input type="text"  className="input_field" placeholder="Contact No."/>
+        </div>
+        <div className="input_container">
+          <input type="text"  className="input_field" placeholder="Email Address"/>
+        </div>
+        <div className="input_container">
+        <select className=" input_field" id="branch" name="branch" required>
+            <option selected>branch</option>
+            <option >CSE</option>
+            <option >CSE(DS)</option>
+            <option >CSE(AI&ML)</option>
+            <option >CS</option>
+            <option >CS&IT</option>
+            <option >IT</option>
+            <option >ECE</option>
+            <option >EN</option>
+            <option >ME</option>
+            <option >CIVIL</option>
+        </select>
+        </div>
+
+        <div className="input_container">
+        <select class="input_field" >
+              <option selected>gender</option>
+              <option value="1">Male</option>
+              <option value="2">Female</option>
+              <option value="2">Other</option>
+              <option value="3">prefer not to say</option>
+        </select>
+          </div>
+          <div className="d-flex justify">
+            
+              <div className="form-check radio">
+                hosteler
+                <input type="radio" className="form-check-input bg-dark " name="gender" required
+                  value="male"
+                   />
+              </div>
+
+              <div className="form-check  radio">
+              Day-Scholar
+                <input type="radio" className="form-check-input bg-dark " name="gender" required
+                  value="female"
+                   />
+
+              </div>
+            </div>
+
+</form>
+          
+          </div>
           
         </div>
-      </div>
     </>
   );
 }
