20570ff9db5e034d6726278f017b93cb11c4ba62
Manish-rai-dev <raimanishkumar52@gmail.com>
2022-04-16 23:39:42 +0530

    validation done with functions

--- src/components/Reg.jsx
+++ src/components/Reg.jsx
@@ -1,220 +1,187 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import validation from "./validation";
-import Axios from "axios";
-function Reg() {
-  const resi = ["Hosteler", "Day-Scholar"];
-  const navigate = useNavigate();
-
-  const [values, setValues] = useState({
-    Name: "",
-    Rollno: "",
-    Contactno: "",
-    Email: "",
-    Gender: "",
-    Year: "",
-    Branch: "",
-    Residence: "",
-  });
-  const [errors, setErrors] = useState({});
-  // console.log(errors);
-  console.log(values.Branch);
-  const handleChange = (event) => {
-    setValues({
-      ...values,
-      [event.target.name]: event.target.value,
-    });
-  };
-  useEffect(() => {
-    if (Object.keys(errors).length === 0) {
-    }
-  }, [errors]);
-  const url = "https://workshopregistration.herokuapp.com/api/users/register";
-  async function handleFormSubmit(event) {
-    console.log(values);
-    event.preventDefault();
-    setErrors(validation(values));
-    const result = await fetch(url, {
-      method: "Post",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-
-      body: JSON.stringify({
-        Name: values.Name,
-        Rollno: values.Rollno,
-        Email: values.Email,
-        Contactno: values.Contactno,
-        Gender: values.Gender,
-        Year: values.Year,
-        Branch: values.Branch,
-        Residence: values.Residence,
-      }),
-    });
-
-    const data = result.json();
-    if (result.status === 400 || !data) {
-      window.alert("enter correct credentials");
-      // console.log("Registered user");
-    } else {
-      Axios.post(
-        "https://workshopregistration.herokuapp.com/api/users/confirmemail",
-        {
-          Email: values.Email,
-        }
-      ).then((res) => {
-        console.log(res.data);
-      });
-      navigate("/");
-    }
-  }
-  return (
-    <>
-      <div className="cont">
-        <div className="image1"></div>
-        <div className="image2">
-          <h2 className="text" style={{}}>Hey! Get Yourself Registered</h2>
-          <form className="form">
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Name"
-                name="Name"
-                value={values.Name}
-                onChange={handleChange}
-              />
-              {errors.Name && <p className="error">{errors.Name}</p>}
-            </div>
-            <div className="form-group">
-              <input
-                type="number"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="University Roll No."
-                name="Rollno"
-                value={values.Rollno}
-                onChange={handleChange}
-              />
-              {errors.Rollno && <p className="error">{errors.Rollno}</p>}
-            </div>
-            <div className="form-group">
-              <input
-                type="number"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Contact No."
-                name="Contactno"
-                value={values.Contactno}
-                onChange={handleChange}
-              />
-              {errors.Contactno && <p className="error">{errors.Contactno}</p>}
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                className="form-control"
-                id="exampleFormControlInput1"
-                placeholder="Email Address"
-                name="Email"
-                value={values.Email}
-                onChange={handleChange}
-              />
-              {errors.Email && <p className="error">{errors.Email}</p>}
-            </div>
-            <div className="form-group">
-              <select
-                name="Branch"
-                value={values.Branch}
-                className="form-control"
-                id="exampleFormControlSelect1"
-                onChange={handleChange}
-              >
-                <option defaultValue="Branch">Branch</option>
-                <option value="CSE" >CSE</option>
-                <option value="CSE(DS)">CSE(DS)</option>
-                <option value="CSE(AI&ML)">CSE(AI&ML)</option>
-                <option value="CS">CS</option>
-                <option value="IT">IT</option>
-                <option value="CS&IT">CS&IT</option>
-                <option value="ECE">ECE</option>
-                <option value="EN">EN</option>
-                <option value="ME">ME</option>
-                <option value="CIVIL">CIVIL</option>
-              </select>
-              {errors.Branch && <p className="error">{errors.Branch}</p>}
-            </div>
-            <div className="form-group">
-              <select
-                name="Year"
-                value={values.Year}
-                className="form-control"
-                id="exampleFormControlSelect1"
-                onChange={handleChange}
-              >
-                <option defaultValue="Year">Year</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-              </select>
-              {errors.Year && <p className="error">{errors.Year}</p>}
-            </div>
-            <div className="form-group">
-              <select
-                name="Gender"
-                value={values.Gender}
-                onChange={handleChange}
-                className="form-control"
-                id="exampleFormControlSelect2"
-              >
-                <option defaultValue="Gender">Gender</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-              {errors.Gender && <p className="error">{errors.Gender}</p>}
-            </div>
-            <div className="form-group">
-              <div className="radio">
-                {resi.map((result) => (
-                  <>
-                    <input
-                      type="radio"
-                      value={result}
-                      className="dot"
-                      name="Residence"
-                      onChange={handleChange}
-                    />
-                    <b className="rad">{result}</b>
-                  </>
-                ))}
-              </div>
-              {errors.Residence && <p className="error">{errors.Residence}</p>}
-            </div>
-            <div
-              className="form-group g-recaptcha"
-              data-sitekey="6LfKURIUAAAAAO50vlwWZkyK_G2ywqE52NU7YO0S"
-            >
-              {/* <input class="form-control d-none" data-recaptcha="true" required data-error="Please complete the Captcha"/> */}
-            </div>
-          </form>
-          <div className="register">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg"
-              onClick={handleFormSubmit}
-              //  onClick={submitform ? navigate("/"): handleFormSubmit}
-            >
-              Register
-            </button>
-          </div>
-=======
 import React, {useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom";
-import validation from './validation';
 import axios from "axios";
 function Reg() {
+  const  [formdata, setFormdata] = useState({
+    Name:"",
+    Rollno:"",
+    Contactno:"",
+    Email:"",
+    Branch:"",
+    Year:"",
+    Gender:"",
+    resi:""
+  });
+
+  var checkStatus=false;
+  var checkStatusAll=false;
+const [formErrors,setFormErrors] =useState({});
+const [formErrorsName,setformErrorsName] =useState({});
+const [formErrorsRoll,setformErrorsRoll] =useState({});
+const [formErrorsContactno,setformErrorsContactno] =useState({});
+const [formErrorsBranch,setformErrorsBranch] =useState({});
+const [isSubmit, setIsSubmit] = useState(false);
+const [focused, setFocused] = useState(false);
+
+
+  let name, value;
+
+  const handleFocus =(e)=>{ 
+    setFocused(true);
+    
+    setFormErrors(validate(formdata));
+  
+  };
+  const handleFocusName =(e)=>{ 
+    setFocused(true);
+    
+    setformErrorsName(validateName(formdata));
+  
+  };
+  const handleFocusRoll =(e)=>{ 
+    setFocused(true);
+    
+    setformErrorsRoll(validateRoll(formdata));
+  
+  };
+  const handleFocusContactno =(e)=>{ 
+    setFocused(true);
+    
+    setformErrorsContactno(validateContactno(formdata));
+  
+  };
+
+  // const handleFocusBranch =(e)=>{ 
+  //   setFocused(true);
+    
+  //   setformErrorsBranch(validateBranch(formdata));
+  
+  // };
+
+  const handleform = (e) =>
+{
+  name = e.target.name;
+  value = e.target.value;
+  setFormdata({...formdata,[name]:[value]});
+}
+const submit = async (e) =>
+{
+ e.preventDefault(); 
+ 
+ setformErrorsName(validateName(formdata));
+ setFormErrors(validate(formdata));
+ setformErrorsContactno(validateContactno(formdata));
+ setformErrorsRoll(validateRoll(formdata));
+setIsSubmit(true);   
+ const {  Name ,  Rollno, Contactno, Email, Branch, Year, Gender, resi} = formdata;
+ const response = await fetch("https://workshopregistration.herokuapp.com/api/users/register",{
+   method: "POST",
+   body:JSON.stringify({
+    Name ,  Rollno, Contactno, Email, Branch, Year, Gender, resi
+   })
+ });
+ const resdata = await response.json();
+ console.log(resdata);
+ const email_res = await fetch("https://workshopregistration.herokuapp.com/api/users/confirmemail",{
+   method:"POST",
+   body:JSON.stringify({
+   Email
+   })
+ });
+ const email_n = await email_res.json();
+ console.log(email_n);
+}
+
+useEffect(() => {
+  console.log(formErrors);
+  if(Object.keys(formErrors).length=== 0 && isSubmit)
+  {
+    console.log(formdata);
+  }
+  }, [formErrors,formdata,isSubmit]);
+
+
+ 
+ 
+  const validate =(value)=> {
+    const errors ={}
+    let regex = new RegExp('[a-z0-9]+@akgec.ac.in'); 
+    
+    if(!value.Email)
+    {
+      errors.Email="email is required!";
+    
+    }
+    
+    
+    else if(!regex.test(value.Email))
+    {
+      errors.Email="This is not a valid email format!";
+      
+    }
+    
+    else{
+      checkStatus=true;
+    }
+    return errors;
+    };
+
+    const validateName =(value)=>{
+const errors ={};
+      if(!value.Name)
+      {
+        errors.Name="Name is required!";
+      
+      }
+     else{
+        checkStatusAll=true;
+      }
+      return errors;
+    };
+    const validateRoll =(value)=>{
+      const errors ={};
+            if(!value.Name)
+            {
+              errors.Rollno="Roll number is required!";
+            
+            }
+           else{
+              checkStatusAll=true;
+            }
+            return errors;
+          };
+
+
+// contact number validation
+
+          const validateContactno =(value)=>{
+            const errors ={};
+                  if(!value.Contactno)
+                  {
+                    errors.Contactno="Contact  number is required!";
+                  
+                  }
+                 else{
+                    checkStatusAll=true;
+                  }
+                  return errors;
+                };
+
+
+                // const validateBranch =(value)=>{
+                //   const errors ={};
+                //         if(!value.Branch)
+                //         {
+                //           errors.Branch="Branch is required!";
+                        
+                //         }
+                //        else{
+                //           checkStatusAll=true;
+                //         }
+                //         return errors;
+                //       };
+      
+
   return (
     <>
       <div className="form_container">
@@ -225,20 +192,25 @@ function Reg() {
           <span className="left_heading"> Get Yourself Registered</span></div>
           <form className="input">
         <div className="input_container">
-          <input type="text"  className="input_field" placeholder="Name"/>
+          <input type="text"  className="input_field" placeholder="Name" name="Name" value={formdata.Name}
+          onChange ={handleform} onBlur={handleFocusName} focused={focused.toString()}/>   <p className='email1'>{formErrorsName.Name}</p>
         </div>
         <div className="input_container">
-          <input type="text"  className="input_field" placeholder="University Roll No."/>
+          <input type="text"  className="input_field" placeholder="University Roll No." name="Rollno" value={formdata.Rollno}
+          onChange ={handleform} onBlur={handleFocusRoll}  focused={focused.toString()}/>   <p className='email1'>{formErrorsRoll.Rollno}</p>
         </div>
         <div className="input_container">
-          <input type="text"  className="input_field" placeholder="Contact No."/>
+          <input type="text"  className="input_field" placeholder="Contact No." name="Contactno" value={formdata.Contactno}
+          onChange ={handleform} onBlur={handleFocusContactno} focused={focused.toString()}/>   <p className='email1'>{formErrorsContactno.Contactno}</p>
         </div>
         <div className="input_container">
-          <input type="text"  className="input_field" placeholder="Email Address"/>
+          <input type="email"  className="input_field emailinp" placeholder="Email Address" name="Email" value={formdata.Email}
+          onChange ={handleform} onBlur={handleFocus} focused={focused.toString()}/>   <p className='email1'>{formErrors.Email}</p>
         </div>
         <div className="input_container">
-        <select className=" input_field" id="branch" name="branch" required>
-            <option selected>branch</option>
+        <select className=" input_field" id="Branch" name="Branch" required value={formdata.Branch}
+          onChange ={handleform} focused={focused.toString()} placeholder="Branch">
+            <option selected>Branch</option>
             <option >CSE</option>
             <option >CSE(DS)</option>
             <option >CSE(AI&ML)</option>
@@ -251,44 +223,48 @@ function Reg() {
             <option >CIVIL</option>
         </select>
         </div>
-
         <div className="input_container">
-        <select class="input_field" >
+        <select className="input_field" name="Gender" value={formdata.Gender}
+          onChange ={handleform}  onBlur={handleFocusName} focused={focused.toString()}>
               <option selected>gender</option>
-              <option value="1">Male</option>
-              <option value="2">Female</option>
-              <option value="2">Other</option>
-              <option value="3">prefer not to say</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="other">Other</option>
+        </select>
+          </div>
+        <div className="input_container">
+        <select className="input_field " name="Year" value={formdata.Year}
+          onChange ={handleform} onBlur={handleFocusName} focused={focused.toString()}>
+              <option selected>Year</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
         </select>
           </div>
           <div className="d-flex justify-content-center">
              <div className="justify">
               <div className= "radio">
               <span className="radio_text">Hosteler</span> 
-                <input type="radio" className="form-check-input bg-blue " name="gender" required
-                  value="Hosteler "
-                   />
-              {/* <span style = {{opacity}}className="radio_text">Hosteler</span> 
-                <input type="radio" onChange={() => setOpacity(1)} className="form-check-input bg-blue " name="gender" required
-                  value="Hosteler "
-                   /> */}
+                <input type="radio" className="form-check-input bg-blue " name="resi" required
+                  value="Hosteler"
+                  onChange ={handleform}/>
+          
               </div>
               <div className="radio">
               <span className="radio_text">Day-Scholar</span> 
-                <input type="radio" className="form-check-input bg-blue " name="gender" required
-                  value="Day-Scholar "
+                <input type="radio" className="form-check-input bg-blue " name="resi" required
+                  value="Day-Scholar"
+                  onChange ={handleform}
                    />
                    </div>
               </div>
               </div>
               <div className="input_container">
-              <button type="button" class="btn input_field">Register</button>
+              <button type="button" class="btn input_field" onClick={submit}>Register</button>
               </div>
 </form>
           
           </div>
           
->>>>>>> 6ede2f183f6b3151dd4f51c3386fa4887c816b48
         </div>
     </>
   );
